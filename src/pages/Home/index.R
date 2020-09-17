b7c5227a5f6f9aa0b45db6b883c00b1643138222
Rebecca Carneiro <rebecca.ufjf@gmail.com>
2020-09-17 10:16:30 -0300

    Dia 2 - primeiras impressôes

---
+++ src/pages/Home/index.R
@@ -0,0 +1,10 @@
+#IndexHome
+
+uiHome <- tags$div(
+  class = "home",
+  sideBarPanel(id = "sideBarPanelHome")
+)
+
+serverHome <- function(input, output, session){
+  sideBarPanelServer(id = "sideBarPanelHome")
+}
